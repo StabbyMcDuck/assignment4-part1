f87989b148bb93a485b858b0753f30523bce673b
Regina <laulessr@gmail.com>
2015-02-08 14:31:39 -0600

    made content1, content2, made html for login.php

---
+++ src/content1.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: reginaimhoff
+ * Date: 2/8/15
+ * Time: 2:30 PM
+ */
+
+?>
--- src/login.php
+++ src/login.php
@@ -4,3 +4,18 @@
  * Date: 2/8/15
  * Time: 2:09 PM
  */
+
+
+
+?>
+
+<html>
+    <form action = "http://web.engr.oregonstate.edu/~imhoffr/assignment4-part1/src/login.php" method = "POST">
+        <label>
+            Username
+                <input type = "text" name = "username"><br>
+        </label>
+        <input type = "submit" value = "Login">
+    </form>
+
+</html>
